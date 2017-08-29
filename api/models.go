303b5014ba9e82b978f40fbcf7e14010f7aa82b8
Dino Hensen <dino.hensen@gmail.com>
2017-08-29 19:03:01 +0200

    chore:add json marshalling meta information

--- api/models.go
+++ api/models.go
@@ -3,25 +3,25 @@ package main
 import uuid "github.com/google/uuid"
 
 type Organisation struct {
-	ID   uuid.UUID
-	Name string
+	ID   uuid.UUID `json:"id"`
+	Name string    `json:"name"`
 }
 
 type User struct {
-	ID       uuid.UUID
-	Name     string
-	Password string
+	ID       uuid.UUID `json:"id"`
+	Name     string    `json:"name"`
+	Password string    `json:"password"`
 }
 
 type Domain struct {
-	ID       uuid.UUID
-	Name     string
-	Provider string
+	ID       uuid.UUID `json:"id"`
+	Name     string    `json:"name"`
+	Provider string    `json:"provider"`
 }
 
 type Domains []Domain
 
 type DomainProvider struct {
-	ID   uuid.UUID
-	Name string
+	ID   uuid.UUID `json:"id"`
+	Name string    `json:"name"`
 }
