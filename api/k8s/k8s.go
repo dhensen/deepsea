a1b055b72c22bc4a00fb8efd03aab05826e2db86
Dino Hensen <dino.hensen@gmail.com>
2017-09-06 13:34:26 +0200

    feat: support in-cluster and outside-cluster usage

--- api/k8s/k8s.go
+++ api/k8s/k8s.go
@@ -6,6 +6,7 @@ import (
 
 	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
@@ -15,13 +16,20 @@ var K8SDiscoveryClient *discovery.DiscoveryClient
 func init() {
 	kubeconfig := flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
 	flag.Parse()
+	var config *rest.Config
+	var err error
 	if *kubeconfig == "" {
-		panic("-kubeconfig not specified")
-	}
-
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	if err != nil {
-		panic(err)
+		// creates the in-cluster config
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		// creates the out of cluster config based on .kube/config file
+		config, err = clientcmd.BuildConfigFromFlags("", *kubeconfig)
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	// set the http client timeout to half a second
