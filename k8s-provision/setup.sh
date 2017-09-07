81035f41ad84fc7ea4d9d3e8f3dc7393208d3ffd
Dino Hensen <dino.hensen@gmail.com>
2017-09-08 01:47:33 +0200

    feat: setup can provision a clean minikube via setup.sh
    At the time of writing: minikube version: v0.22.0

--- k8s-provision/setup.sh
+++ k8s-provision/setup.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-openssl req \
-  -x509 -newkey rsa:2048 -nodes -days 365 \
-  -keyout tls.key -out tls.crt -subj '/CN=localhost'
+minikube ip
+if [ $? -ne 0 ]; then
+  minikube start --insecure-registry "127.0.0.1:5000"
+fi
 
-kubectl create secret tls tls-secret --cert=tls.crt --key=tls.key
+kubectl create -f kube-registry.yaml
+
+# See https://github.com/kubernetes/ingress/tree/master/examples/deployment/haproxy for more information
+
+# if folder secret does not exist create TLS secret
+if [ ! -d secret ]; then
+  mkdir -p secret
+  openssl req \
+    -x509 -newkey rsa:2048 -nodes -days 365 \
+    -keyout tls.key -out tls.crt -subj '/CN=localhost'
+fi
+
+kubectl create secret tls tls-secret --cert=secret/tls.crt --key=secret/tls.key
 
 kubectl run ingress-default-backend \
   --image=gcr.io/google_containers/defaultbackend:1.0 \
@@ -15,6 +28,7 @@ kubectl run ingress-default-backend \
 kubectl create configmap haproxy-ingress
 
 # wget https://github.com/kubernetes/ingress/raw/master/examples/deployment/haproxy/haproxy-ingress.yaml
+# modification: added hostPort for 80,443 and 1936
 kubectl create -f haproxy-ingress.yaml
 
 kubectl expose deploy/haproxy-ingress --type=NodePort
@@ -25,3 +39,18 @@ kubectl create -f mysql.yaml
 # Everything inside /data is persisted across minikube reboot.
 # Beware: ofcourse this does not scale and PV uses hostPath is only for development purposes
 minikube ssh "sudo mkdir -p /data/volumes && sudo chown -R docker:docker /data/volumes"
+
+docker build -t deepsea:v1 $PWD/../
+docker tag deepsea:v1 192.168.99.100:5000/deepsea:v1
+
+status=$(kubectl get pods | grep registry | awk '{print $3;}')
+while [ "$status" != 'Running' ];
+do
+  echo "waiting for registry to be ready before pushing image"
+  sleep 2
+  status=$(kubectl get pods | grep registry | awk '{print $3;}')
+done
+
+docker push 192.168.99.100:5000/deepsea:v1
+
+kubectl create -f $PWD/../deepsea.yaml
